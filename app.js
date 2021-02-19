7a133e44933d61d9dd6c7d88b84aaa32429443f4
@lukeocodes <luke@lukeoliff.com>
2021-02-19 15:35:11 +0000

    feat(server): remove cookie parser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
@@ -10,7 +9,6 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/dist")));
